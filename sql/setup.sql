4f22d85300da2132c9f4926e002d4ee5dd17b0c5
Amanda Michelle Hecht <amandahecht89@gmail.com>
2023-01-25 16:23:56 -0700

    csetup db, controller and model added for getbyid

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table if exists storyline;
+
+create table storyline (
+    id bigint generated always as identity primary key,
+    page_number bigint not null,
+    story varchar not null,
+    photo varchar not null
+);
+
+insert into storyline (page_number, story, photo) values
+(1, 'yyjyfnd', 'cxriixryixry0');
